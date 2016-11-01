f3f4971bbd4b2f88b6d0a5d49c63ea4d342367df
lorenzo <l.romagnoli@arduino.cc>
2016-11-01 10:46:29 +0800

    Updates

--- js/script.js
+++ js/script.js
@@ -12,4 +12,31 @@ window.onload = function() {
     $('body').scrollspy({target: ".navbar"});
     AOS.init();
 
+    $('#hero em').replaceWith($('#hero .replacement_verbs'));
+
+
+    setInterval(function(){
+      randomTextInHero();
+      console.log("update");
+    }, 3000);
+
+    $('#hero .claim ul').click(function(){
+      randomTextInHero();
+    });
+
 }
+
+var randomTextInHero= function(){
+  var replacement_verbs=$('#hero .replacement_verbs li');
+  var randomtop=$('#hero .claim ul li').outerHeight() * Math.floor(Math.random()*replacement_verbs.length)+9;
+  $('#hero .claim ul li').css("top",randomtop*-1); //modify top position
+}
+
+
+/*
+  var replacement_verbs=$('#hero .replacement_verbs li');
+  var randomName=replacement_verbs.eq(Math.floor(Math.random()*replacement_verbs.length)).text();
+  console.log(randomName);
+  $('#hero em').html(randomName);
+  console.log(replacement_verbs);
+*/
