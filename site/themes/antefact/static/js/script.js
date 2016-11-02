878647ff64f0d267715d6add74704ed0f0c81433
lorenzo <l.romagnoli@arduino.cc>
2016-11-02 13:11:17 +0800

    added code to animate svg

--- site/themes/antefact/static/js/script.js
+++ site/themes/antefact/static/js/script.js
@@ -1,29 +1,49 @@
 var $root = $('html, body');
+
+//this make the browser scroll to a specific anchor
 $('a').click(function() {
     $root.animate({
         scrollTop: $($.attr(this, 'href')).offset().top
           }, 500);
     return false;
 });
-var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+
 
 window.onload = function() {
+
     //add scrollspy to activate menu on scroll
     $('body').scrollspy({target: ".navbar"});
 
+    //when we get to a new section we check if there are things to do
+    $('body').on('activate.bs.scrollspy', function () {
+      var currentSection = $(".nav li.active > a").attr('href');
+
+      //if we are in process animation, restart the animation
+      if (currentSection==="#process-animation"){
+        processAnim.stop().reset().play(2);
+      }
+
+    })
+
+    //init the plugin for the animation when you scroll on to something
     AOS.init();
 
+    //if js works replace a word from the claim with the full list of options
     $('#hero em').replaceWith($('#hero .replacement_verbs'));
 
+    //start changing the text in the hero-claim
     randomTextInHero();
-    setInterval(function(){
-      randomTextInHero();
-    }, 3000);
+    setInterval(function(){randomTextInHero()}, 3000);
     $('#hero .claim ul').click(function(){
       randomTextInHero();
     });
 
+    //load the asset fot the animation
+    var processAnim =new Vivus('process-animation', {duration: 200, file: 'svg/process.svg'}, function(){});
 }
+
+
 
 var randomTextInHero= function(){
   var replacement_verbs=$('#hero .replacement_verbs li');
